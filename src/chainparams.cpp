0245e05e719db2d3e6c228deec34abae48274c61
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2021-12-29 11:55:26 +0100

    Adding master seednode

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -159,10 +159,11 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
+	vSeeds.emplace_back("seed.revo.network"); // Revo mainnet
         vSeeds.emplace_back("seed1.revo.network"); // Revo mainnet
-		vSeeds.emplace_back("seed2.revo.network"); // Revo mainnet
-		vSeeds.emplace_back("seed3.revo.network"); // Revo mainnet
-		vSeeds.emplace_back("seed4.revo.network"); // Revo mainnet
+	vSeeds.emplace_back("seed2.revo.network"); // Revo mainnet
+	vSeeds.emplace_back("seed3.revo.network"); // Revo mainnet
+	vSeeds.emplace_back("seed4.revo.network"); // Revo mainnet
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,60);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,50);
