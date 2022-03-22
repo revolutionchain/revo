8d72c10d1a50bef5b7679d830955ec85dac40b06
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2022-03-22 13:35:42 +0100

    Update Default DGP values

--- src/revo/revoDGP.h
+++ src/revo/revoDGP.h
@@ -12,17 +12,17 @@ static const dev::Address GasPriceDGP = dev::Address("00000000000000000000000000
 static const dev::Address DGPCONTRACT4 = dev::Address("0000000000000000000000000000000000000083");
 static const dev::Address BlockGasLimitDGP = dev::Address("0000000000000000000000000000000000000084");
 
-static const uint32_t MIN_BLOCK_SIZE_DGP = 500000;
+static const uint32_t MIN_BLOCK_SIZE_DGP = 1000000;
 static const uint32_t MAX_BLOCK_SIZE_DGP = 32000000;
 static const uint32_t DEFAULT_BLOCK_SIZE_DGP = 2000000;
 
 static const uint64_t MIN_MIN_GAS_PRICE_DGP = 1;
 static const uint64_t MAX_MIN_GAS_PRICE_DGP = 10000;
-static const uint64_t DEFAULT_MIN_GAS_PRICE_DGP = 40;
+static const uint64_t DEFAULT_MIN_GAS_PRICE_DGP = 1;
 
 static const uint64_t MIN_BLOCK_GAS_LIMIT_DGP = 1000000;
 static const uint64_t MAX_BLOCK_GAS_LIMIT_DGP = 1000000000;
-static const uint64_t DEFAULT_BLOCK_GAS_LIMIT_DGP = 40000000;
+static const uint64_t DEFAULT_BLOCK_GAS_LIMIT_DGP = 1000000000;
 
 class RevoDGP {
     
