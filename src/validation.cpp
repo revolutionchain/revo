yodynetwork/yody

add createdContracts and destructedContracts

src/validation.cpp
@@ -2482,7 +2482,11 @@ bool ByteCodeExec::performByteCode(dev::eth::Permanence type){
         if(!tx.isCreation() && !globalState->addressInUse(tx.receiveAddress())){
             dev::eth::ExecutionResult execRes;
             execRes.excepted = dev::eth::TransactionException::Unknown;
-            result.push_back(ResultExecute{execRes, QtumTransactionReceipt(dev::h256(), dev::h256(), dev::u256(), dev::eth::LogEntries()), CTransaction()});
+            result.push_back(ResultExecute{
+                execRes,
+                QtumTransactionReceipt(dev::h256(), dev::h256(), dev::u256(), dev::eth::LogEntries(), {}, {}),
+                CTransaction()
+            });
             continue;
         }
         result.push_back(globalState->execute(envInfo, *globalSealEngine.get(), tx, chain, type, OnOpFunc()));
@@ -3220,6 +3224,8 @@ bool CChainState::ConnectBlock(const CBlock& block, BlockValidationState& state,
                         resultExec[k].txRec.bloom(),
                         resultExec[k].txRec.stateRoot(),
                         resultExec[k].txRec.utxoRoot(),
+                        resultExec[k].txRec.createdContracts(),
+                        resultExec[k].txRec.destructedContracts()
                     });
                 }
 

revolutionchain/revo

src/validation.cpp
@@ -2547,7 +2547,11 @@ bool ByteCodeExec::performByteCode(dev::eth::Permanence type){
         if(!tx.isCreation() && !globalState->addressInUse(tx.receiveAddress())){
             dev::eth::ExecutionResult execRes;
             execRes.excepted = dev::eth::TransactionException::Unknown;
-            result.push_back(ResultExecute{execRes, RevoTransactionReceipt(dev::h256(), dev::h256(), dev::u256(), dev::eth::LogEntries()), CTransaction()});
+            result.push_back(ResultExecute{
+                execRes,
+                RevoTransactionReceipt(dev::h256(), dev::h256(), dev::u256(), dev::eth::LogEntries(), {}, {}),
+                CTransaction()
+            });
             continue;
         }
         result.push_back(globalState->execute(envInfo, *globalSealEngine.get(), tx, chain, type, OnOpFunc()));
@@ -3285,6 +3289,8 @@ bool CChainState::ConnectBlock(const CBlock& block, BlockValidationState& state,
                         resultExec[k].txRec.bloom(),
                         resultExec[k].txRec.stateRoot(),
                         resultExec[k].txRec.utxoRoot(),
+                        resultExec[k].txRec.createdContracts(),
+                        resultExec[k].txRec.destructedContracts()
                     });
                 }
 